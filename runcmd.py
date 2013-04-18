7b0a5ce74e5d439969112866ee3d8051aee18cc3
Tobit Raff <0xtobit@gmail.com>
2013-04-18 19:38:43 -0400

    Reorganized runcmd to have a runcmd, check_cmd fcn

--- runcmd.py
+++ runcmd.py
@@ -7,15 +7,16 @@ class CalledProcessError(SubprocessError):
     The exit status will be stored in the returncode attribute;
     check_output() will also store the output in the output attribute.
     """
-    def __init__(self, returncode, cmd, output=None):
+    def __init__(self, returncode, cmd, output=None, error=None):
         self.returncode = returncode
         self.cmd = cmd
         self.output = output
+        self.error = error
     def __str__(self):
         return "Command '%s' returned non-zero exit status %d" % (self.cmd, self.returncode)
 
 
-def runcmd(*popenargs, **kwargs):
+def check_cmd(*popenargs, **kwargs):
     """Run command with arguments.  Wait for command to complete.  If
     the exit code was zero then return, otherwise raise
     CalledProcessError.  The CalledProcessError object will have the
@@ -37,6 +38,28 @@ def runcmd(*popenargs, **kwargs):
         cmd = kwargs.get("args")
         if cmd is None:
             cmd = popenargs[0]
-        raise CalledProcessError(retcode, cmd, out)
-    return 0
+        raise CalledProcessError(retcode, cmd, out, err)
+    return out, err
+
+def runcmd(*popenargs, **kwargs):
+    """Run command with check_call. If the command returns 1, print stdout and
+    stderr. IF verbose, print the command, the stdout, else return the stdout
+    for (probable) parsing.
+    """
+    if 'verbose' in kwargs:
+        print(''.join(popenargs[0]))
+        verbose = True
+    else:
+        verbose = False
+    try:
+        out, err = check_call(*popenargs, **kwargs)
+    except CalledProcessError as e:
+        print(e)
+        print(e.output)
+        print(e.error)
+        exit(1)
+    if verbose:
+        print(out)
+        print(err)
+    return out, err
 
